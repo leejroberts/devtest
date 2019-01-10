1c460d2dc3e71422519d6882fdece5542779fa82
leejroberts <leejamesroberts@gmail.com>
2019-01-10 15:29:34 -0500

    parens_matcher is complete

--- parentheses_matcher.rb
+++ parentheses_matcher.rb
@@ -1,5 +1,23 @@
 class ParenthesesMatcher
+
   def self.match(str, idx)
-    # TODO
+    return -1 if "(" != str[idx]
+    parenstack = []
+
+    str.each_char.with_index do |char, i|
+      if i <= idx
+        next
+      elsif char == "("
+        parenstack << char
+      elsif char == ")"
+        if parenstack.empty?
+          return i
+        else
+          parenstack.pop
+        end
+      end
+    end
+    -1
   end
+
 end
